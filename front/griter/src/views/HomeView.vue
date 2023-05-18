666b81339227eb70b8350eb791f56441d1c7c762
Jaemin-Park9708 <catbirdseat@naver.com>
2023-05-19 02:37:57 +0900

    230519 02:37 pjm commit

--- front/griter/src/views/HomeView.vue
+++ front/griter/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <aside-nav />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import AsideNav from '@/components/common/AsideNav.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    AsideNav
   }
 }
 </script>
