21aa2b183b7cf4ddd6a4b2e3b6d0acceb7b70fb2
Jaemin-Park9708 <catbirdseat@naver.com>
2023-05-24 02:56:47 +0900

    230524 02:56 pjm commit

--- front/griter/src/components/posts/PostsCreate.vue
+++ front/griter/src/components/posts/PostsCreate.vue
@@ -3,25 +3,74 @@
     <div class="postcreate">
       <div class="postcreate-header">
         <i class="bx bx-arrow-back" id="back-btn" @click="moveList"></i>
-        <span id="postcreate-header-logo">New Post</span>
+        <span v-if="this.routerName === 'PostModify'" id="postcreate-header-logo"
+          >Modify Post</span
+        >
+        <span v-else id="postcreate-header-logo">New Post</span>
       </div>
       <div class="line"></div>
       <div class="postcreate-content">
         <fieldset class="create-form">
           <div class="label">Title</div>
-          <input type="text" id="title" v-model="title" class="form-control" placeholder="Enter title here"
-            required /><br />
+          <input
+            v-if="this.routerName === 'PostModify'"
+            type="text"
+            id="title"
+            v-model="post[0].title"
+            class="form-control"
+            required
+            maxlength="50"
+          />
+          <input
+            v-else
+            type="text"
+            id="title"
+            v-model="title"
+            class="form-control"
+            placeholder="Enter title here"
+            required
+            maxlength="50"
+          /><br />
           <div class="label">Category</div>
-          <select id="category" v-model="category" class="form-select" aria-label="Default select example">
+          <select
+            id="category"
+            v-model="category"
+            class="form-select"
+            aria-label="Default select example"
+          >
             <option selected hidden>Select</option>
             <option value="routine">Routine</option>
             <option value="diet">Diet</option>
             <option value="etc">Something Else</option>
           </select>
           <div class="label">Content</div>
-          <textarea type="text" id="content" v-model="content" class="form-control" placeholder="Enter content here"
-            required /><br />
-          <button class="btn btn-primary" @click="create" id="post-btn">POST</button>
+          <textarea
+            v-if="this.routerName === 'PostModify'"
+            type="text"
+            id="content"
+            v-model="post[0].content"
+            class="form-control"
+            placeholder="Enter content here"
+            required
+          />
+          <textarea
+            v-else
+            type="text"
+            id="content"
+            v-model="content"
+            class="form-control"
+            placeholder="Enter content here"
+            required
+          /><br />
+          <div
+            v-if="this.routerName === 'PostModify'"
+            class="btn btn-primary"
+            @click="update"
+            id="post-btn"
+          >
+            UPDATE
+          </div>
+          <div v-else class="btn btn-primary" @click="create" id="post-btn">POST</div>
         </fieldset>
       </div>
     </div>
@@ -29,7 +78,7 @@
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex';
+import { mapActions, mapState } from "vuex";
 
 export default {
   name: "createPost",
@@ -39,16 +88,18 @@ export default {
       content: "",
       category: "",
       img: [],
+      routerName: "",
     };
   },
   computed: {
-    ...mapState('userModule', ["loginUser"]),
+    ...mapState("userModule", ["loginUser"]),
+    ...mapState("postModule", ["post"]),
   },
   methods: {
     create() {
       const user_id = localStorage.getItem("loginUser");
       console.log(user_id);
-      console.log(this.title)
+      console.log(this.title);
       console.log(this.content);
       console.log(this.category);
       if (user_id.length > 0 && this.title.length > 0 && this.content.length > 0) {
@@ -58,20 +109,51 @@ export default {
           content: this.content,
           category: this.category,
         });
-      } else{
+      } else {
         alert("필수항목을 모두 입력해주세요");
       }
     },
     moveList() {
       this.$router.push("/posts");
     },
-    ...mapActions('userModule', ["getLoginUser"]),
-    ...mapActions('postModule', ["createPost"]),
+    ...mapActions("userModule", ["getLoginUser"]),
+    ...mapActions("postModule", ["createPost", "getPost", "updatePost"]),
+    update() {
+      const user_id = localStorage.getItem("loginUser");
+      console.log(user_id);
+      console.log(this.post[0].post_id);
+      console.log(this.post[0].title);
+      console.log(this.post[0].content);
+      console.log(this.post[0].category);
+      if (
+        user_id.length > 0 &&
+        this.post[0].title.length > 0 &&
+        this.post[0].content.length > 0 &&
+        this.post[0].category.length > 0
+      ) {
+        this.updatePost({
+          post_id: this.post[0].post_id,
+          user_id: user_id,
+          title: this.post[0].title,
+          content: this.post[0].content,
+          category: this.post[0].category,
+        });
+      } else {
+        alert("필수항목을 모두 입력해주세요");
+      }
+    },
   },
   created() {
     const user_id = localStorage.getItem("loginUser");
     this.getLoginUser(user_id);
-  }
+    this.routerName = this.$router.currentRoute.name;
+    // console.log(this.$router.currentRoute.params.post_id);
+    // console.log(this.routerName);
+    if (this.routerName === "PostModify") {
+      const post_id = this.$router.currentRoute.params.post_id;
+      this.getPost(post_id);
+    }
+  },
 };
 </script>
 <style scoped>
